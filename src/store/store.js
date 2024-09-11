c5c52332885f89d4fede62aae1c0672bf176079b
Kirill <kirilltanae314@gmail.com>
2024-09-11 16:19:43 +0600

    init step 2

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+
+});
+
+export const store = configureStore({});
