96d5e4faf82985c90748dd3a41119ac6ecb644fa
Kirill <kirilltanae314@gmail.com>
2024-11-13 00:51:37 +0600

    refactoring Tabs component

--- src/components/BurgerIngredients/Tabs/Tabs.tsx
+++ src/components/BurgerIngredients/Tabs/Tabs.tsx
@@ -3,38 +3,35 @@ import { memo } from 'react';
 import cls from './Tabs.module.css';
 import { IngredientType } from '@/shared/types/api';
 
+interface Tab {
+  name: IngredientType;
+  href: string;
+}
+
 interface Props {
   currentTab: IngredientType;
 }
 
 export const Tabs = memo(({ currentTab }: Props) => {
+  const tabs: Tab[] = [
+    { name: 'bun', href: '#bun' },
+    { name: 'sauce', href: '#sauce' },
+    { name: 'main', href: '#main' },
+  ];
+
   return (
     <nav className='mb-10'>
       <ul className={cls.nav_list}>
-        <li className={cls.nav_item}>
-          <a href='#bun'>
-            {/* @ts-expect-error onClick is required */}
-            <Tab value='bun' active={currentTab === 'bun'}>
-              Булки
-            </Tab>
-          </a>
-        </li>
-        <li className={cls.nav_item}>
-          <a href='#sauce'>
-            {/* @ts-expect-error onClick is required */}
-            <Tab value='sauce' active={currentTab === 'sauce'}>
-              Соусы
-            </Tab>
-          </a>
-        </li>
-        <li className={cls.nav_item}>
-          <a href='#main'>
-            {/* @ts-expect-error onClick is required */}
-            <Tab value='main' active={currentTab === 'main'}>
-              Начинки
-            </Tab>
-          </a>
-        </li>
+        {tabs.map(({ name, href }) => (
+          <li className={cls.nav_item}>
+            <a href={href}>
+              {/* @ts-expect-error onClick is required */}
+              <Tab value={name} active={currentTab === name}>
+                Булки
+              </Tab>
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
