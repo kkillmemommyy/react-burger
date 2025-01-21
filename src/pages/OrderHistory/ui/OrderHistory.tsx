6db0dd0535820402fcc529672755a00a0e73be9e
Kirill <kirilltanae314@gmail.com>
2025-01-21 08:44:11 +0600

    little fixes

--- src/pages/OrderHistory/ui/OrderHistory.tsx
+++ src/pages/OrderHistory/ui/OrderHistory.tsx
@@ -4,22 +4,36 @@ import { Loader } from '@/shared/ui/Loader';
 import { PageErrorMessage } from '@/shared/ui/PageErrorMessage';
 import { Outlet } from 'react-router-dom';
 import { useGetUserOrdersQuery } from '@/shared/api/user/userApi';
+import clsx from 'clsx';
 
 const OrderHistory = () => {
   const { data, isLoading, isError, isSuccess } = useGetUserOrdersQuery();
 
   if (isLoading) {
-    return <Loader center={false} className={cls.loader} type='text' text='Загружаем ваши заказы' />;
+    return (
+      <Loader
+        center={false} 
+        className={clsx(cls.loader, cls.flex_center)}
+        type='text'
+        text='Загружаем ваши заказы'
+      />
+    );
   }
 
   if (isError || !isSuccess) {
     return <PageErrorMessage />;
   }
 
+  const hasOrderHistory = data.orders.length > 0;
+
   return (
     <>
-      <main className={cls.main}>
-        <OrderFeed orders={data.orders} />
+      <main className={clsx(cls.main, { [cls.flex_center]: !hasOrderHistory })}>
+        {hasOrderHistory ? (
+          <OrderFeed orders={data.orders} />
+        ) : (
+          <p className={'text text_type_main-medium'}>История пуста</p>
+        )}
       </main>
       <Outlet />
     </>
